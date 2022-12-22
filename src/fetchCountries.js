ec6d28b07f48293dce5d4ad872ac5fbde2b5f3ed
nadikkk <109106413+nadikkk@users.noreply.github.com>
2022-12-22 20:54:36 +0200

    ++

---
+++ src/fetchCountries.js
@@ -0,0 +1,5 @@
+function fetchCountries(name) {
+	return fetch('https://restcountries.com/v3.1/name/${name}')
+	.then(response => response.json());
+}
+export default {fetchCountries};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './css/styles.css';
-
+// import API from "./fetchCountries";
 const DEBOUNCE_DELAY = 300;
+const nameCountry = 'peru';
+// function fetchCountries(name) {
+	
+// }
+fetch('https://restcountries.com/v3.1/name/${nameCountry}')
+.then(res => 	res.json())
+.then(r => console.log(r))
+ console.log('https://restcountries.com/v3.1/name/${nameCountry}');
+ console.log(typeof nameCountry, nameCountry, '${nameCountry}');
