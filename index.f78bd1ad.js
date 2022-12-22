9cbafeb7552b331f23fbd094e695a25da61b0d9d
nadikkk <109106413+nadikkk@users.noreply.github.com>
2022-12-22 23:47:51 +0000

    Deploying to gh-pages from @ nadikkk/goit-js-hw-10@cff044121be9d8090636fb1e91c2adb425f9aa8b 🚀

--- index.f78bd1ad.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/${nameCountry}").then((n=>n.json())).then((n=>console.log(n))),console.log("https://restcountries.com/v3.1/name/${nameCountry}"),console.log("string","peru","${nameCountry}");
-//# sourceMappingURL=index.f78bd1ad.js.map
